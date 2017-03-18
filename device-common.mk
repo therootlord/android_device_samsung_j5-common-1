033f99d028ba62b7c0758780221a8632b603601c
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-03-18 00:47:02 -0400

    gprimelte: Move props to system_prop.mk

--- device-common.mk
+++ device-common.mk
@@ -20,6 +20,9 @@ $(call inherit-product, device/samsung/msm8916-common/msm8916.mk)
 
 LOCAL_PATH := device/samsung/gprimelte-common
 
+# System properties
+-include $(LOCAL_PATH)/system_prop.mk
+
 # Common overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
@@ -75,14 +78,6 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/keylayout/sec_touchscreen.kl:system/usr/keylayout/sec_touchscreen.kl \
 	$(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
 
-# Default Property Overrides
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-	rild.libpath=/system/lib/libsec-ril.so \
-	persist.radio.lte_vrte_ltd=1 \
-	persist.radio.add_power_save=1 \
-	camera2.portability.force_api=1 \
-	ro.secure=0
-
 # Configuration files
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/Diag.cfg:system/etc/Diag.cfg \
@@ -103,20 +98,6 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
 	frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
 	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
-
-# Properties
-PRODUCT_PROPERTY_OVERRIDES += \
-	qcom.hw.aac.encoder=true \
-	media.stagefright.legacyencoder=true \
-	media.stagefright.less-secure=true \
-	ro.use_data_netmgrd=false \
-	persist.radio.sib16_support=1 \
-	ro.gps.agps_provider=1 \
-	ro.pip.gated=0 \
-	ro.config.vc_call_vol_steps=15 \
-	ro.config.media_vol_steps=30 \
-	ro.telephony.samsung.realcall=true \
-	ro.telephony.ril_class=SamsungQcomRIL
 
 # append the updater uri to the product properties if set
 ifneq ($(CM_UPDATER_OTA_URI),)
