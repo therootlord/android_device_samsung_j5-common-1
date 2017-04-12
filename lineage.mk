af5db6dbbd9d5d93b8fb9d5065d8b6958929ff65
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-04-12 03:12:36 -0400

    j5: Reconfigure tree for J5 devices
    * Change Makefile guards
    * Update releasetools to reflect new device names
    * Rename and update libinit
    * Update device names
    * Update base configs
    * Update bluedroid config
    * Update README

--- lineage.mk
+++ lineage.mk
@@ -1,10 +1,10 @@
 # Release name
-PRODUCT_RELEASE_NAME := Samsung Galaxy Grand Prime
+PRODUCT_RELEASE_NAME := Samsung Galaxy J5
 CM_BUILDTYPE := NIGHTLY
 
 # Boot animation
-TARGET_SCREEN_WIDTH := 540
-TARGET_SCREEN_HEIGHT := 960
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
