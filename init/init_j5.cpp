9cb465e2c9754a46c64b234c17bde6a3b2645284
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-04-14 12:04:17 -0400

    init: Add j5lte and j5nlte

--- init/init_j5.cpp
+++ init/init_j5.cpp
@@ -53,12 +53,34 @@ void cdma_properties(char const *operator_alpha,
 	property_set("telephony.lteOnCdmaDevice", "1");
 }
 
+void gsm_lte_properties(char const * default_network)
+{
+	property_set("persist.radio.lte_vrte_ltd", "1");
+	property_set("telephony.lteOnCdmaDevice", "0");
+	property_set("ro.telephony.default_network", default_network);
+}
 
 void init_target_properties(void)
 {
 	std::string bootloader = property_get("ro.bootloader");
 
-	if (bootloader.find("J500H") == 0) {
+	if (bootloader.find("J500FN") == 0) {
+		property_set("ro.build.product", "j5nlte");
+		property_set("ro.build.description", "lineage_j5nlte-userdebug 7.1.1 NMF26V 4dae919457 test-keys");
+		property_set("ro.build.fingerprint", "samsung/lineage_j5nlte/j5nlte:7.1.1/NMF26V/4dae919457:userdebug/test-keys");
+		property_set("ro.product.device", "j5nlte");
+		property_set("ro.product.model", "SM-J500FN");
+		gsm_lte_properties("10");
+	}
+	else if (bootloader.find("J500F") == 0) {
+		property_set("ro.build.product", "j5lte");
+		property_set("ro.build.description", "lineage_j5lte-userdebug 7.1.1 NMF26V 4dae919457 test-keys");
+		property_set("ro.build.fingerprint", "samsung/lineage_j5lte/j5lte:7.1.1/NMF26V/4dae919457:userdebug/test-keys");
+		property_set("ro.product.device", "j5lte");
+		property_set("ro.product.model", "SM-J500F");
+		gsm_lte_properties("10");
+	}
+	else if (bootloader.find("J500H") == 0) {
 		property_set("ro.build.product", "j53gxx");
 		property_set("ro.build.description", "lineage_j53gxx-userdebug 7.1.1 NMF26V 4dae919457 test-keys");
 		property_set("ro.build.fingerprint", "samsung/lineage_j53gxx/j53gxx:7.1.1/NMF26V/4dae919457:userdebug/test-keys");
