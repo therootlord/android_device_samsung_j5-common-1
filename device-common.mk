5366064d174dce09f55dbe200fc22566847090cc
GeneticEnginer <igor_cestari@hotmail.com>
2017-05-27 02:51:01 -0300

    Fix Nfc #2

--- device-common.mk
+++ device-common.mk
@@ -89,7 +89,6 @@ PRODUCT_PACKAGES += \
 # OTA scripts
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/releasetools/variant_hook.sh:install/bin/variant_hook.sh \
-	$(LOCAL_PATH)/releasetools/installend/copy_nfc_configs.sh:install/bin/installend/copy_nfc_configs.sh \
 	$(LOCAL_PATH)/releasetools/installend/copy_variant_blobs.sh:install/bin/installend/copy_variant_blobs.sh \
 	$(LOCAL_PATH)/releasetools/installend/update_baseband.sh:install/bin/installend/update_baseband.sh \
 	$(LOCAL_PATH)/releasetools/installend/update_device_name.sh:install/bin/installend/update_device_name.sh
@@ -129,3 +128,4 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 # Inhert dalvik heap values from aosp
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
